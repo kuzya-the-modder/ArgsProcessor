0b16971a9dc16c39e53d1d3eb5e0c3c83bc77a26
kuzya-the-modder <kuzyathemodder@gmail.com>
2024-04-27 20:06:46 +0300

    Updated v1.1a

--- ArgsProcessor/Args.cs
+++ ArgsProcessor/Args.cs
@@ -1,10 +1,10 @@
-﻿namespace ArgsProcessor;
+﻿namespace Kuzya;
 
 public class Args
 { // Wrapper Dictionary<string,string>
     Dictionary<string, string> self;
     public Args(string[] args) {
-        self = Kuzya.Args.Parse(args);
+        self = Parse(args);
     }
     public string? GetArg(string name)
     {
@@ -26,4 +26,37 @@ public class Args
         foreach (var name in aliases) if (HasFlag(name)) return true;
         return false;
     }
+    public static Dictionary<string, string> Parse(string[] args)
+    {
+        string? name = null;
+        var kvs = new Dictionary<string, string>();
+        foreach (var arg in args)
+        {
+            // arg[0] == '-' -> name
+            // else -> value 
+            if (arg[0] == '-')
+            {
+                // name
+                if (name is not null)
+                {
+                    // fix flags
+                    kvs.Add(name, string.Empty);
+                }
+                name = arg[1..(arg.Length)];
+            }
+            else
+            {
+                // value
+                if (name is null) continue; //throw new InvalidArgsFormatException("Invalid Argument Typo");
+                try
+                {
+                    kvs.Add(name, arg);
+                }
+                catch (ArgumentException) { }
+                name = null;
+            }
+        }
+        return kvs;
+    }
+    public static Args New(string[] args) => new Args(args);
 }
