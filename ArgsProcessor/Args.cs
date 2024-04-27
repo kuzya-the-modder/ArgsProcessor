e9bc3a19f339bd013a6969c40c815821e61f3a43
kuzya-the-modder <egorbespalov385@gmail.com>
2024-04-27 18:53:46 +0300

    Add project files.

---
+++ ArgsProcessor/Args.cs
@@ -0,0 +1,29 @@
+﻿namespace ArgsProcessor;
+
+public class Args
+{ // Wrapper Dictionary<string,string>
+    Dictionary<string, string> self;
+    public Args(string[] args) {
+        self = Kuzya.Args.Parse(args);
+    }
+    public string? GetArg(string name)
+    {
+        if (self.TryGetValue(name, out var value)) return value;
+        return null;
+    }
+    public string? GetArg(params string[] aliases)
+    {
+        foreach (var name in aliases) if (self.TryGetValue(name, out var value)) return value;
+        return null;
+    }
+    public bool HasFlag(string name)
+    {
+        var value = GetArg(name);
+        return value is not null && value == string.Empty;
+    }
+    public bool HasFlag(params string[] aliases)
+    {
+        foreach (var name in aliases) if (HasFlag(name)) return true;
+        return false;
+    }
+}
---
+++ Debuging/Program.cs
@@ -0,0 +1,7 @@
+﻿public static class Debug {
+    public static void Main() {
+        string[] argvs = {"-hf","-hello","world" };
+        var args = Kuzya.Args.Parse(argvs);
+        Console.WriteLine(string.Join(' ', args));
+    }
+}
