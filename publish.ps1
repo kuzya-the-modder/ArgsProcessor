d89d1c1c7679cc355c344251e30639272414361c
Kuzya <kuzyathemodder@gmail.com>
2024-05-10 08:10:35 +0300

    Deleted publish.ps1

--- publish.ps1
+++
@@ -1,2 +0,0 @@
-dotnet publish ArgsProcessor/ArgsProcessor.csproj
-move D:\Programming\Lang\C#\Projs\ArgsProcessor\ArgsProcessor\bin\Release\net8.0\publish\ArgsProcessor.dll D:\PScripts\ArgsProcessor.dll -force
